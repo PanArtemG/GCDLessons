4c1280512da584ba6a9181600922d7fe93ab5eed
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-17 18:24:30 +0200

    interface

---
+++ GSD/GSD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GSD
+//
+//  Created by Artem Panasenko on 17.03.2020.
+//  Copyright © 2020 Artem Panasenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
