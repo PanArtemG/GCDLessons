a316b7d3d9b92fb0baf023c4f1533de0124dce98
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-18 17:33:54 +0200

    Group

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let queue = DispatchQueue(label: "swift", attributes: .concurrent)
+let group = DispatchGroup()
+
+queue.async(group: group) {
+    for i in 0...10 {
+        if i == 10 {
+            print(i)
+        }
+    }
+}
+
+queue.async(group: group) {
+    for i in 0...20 {
+        if i == 20 {
+            print(i)
+        }
+    }
+}
+
+
+group.notify(queue: .main) { //notify - меод уведомления
+    print("First completed")
+}
+
+
+let secondGroup = DispatchGroup()
+secondGroup.enter()
+queue.async(group: group) {
+    for i in 0...30 {
+        if i == 30 {
+            print(i)
+            secondGroup.leave()
+        }
+    }
+}
+
+let result = secondGroup.wait(timeout: .now() + 1)
+print(result)
+
+secondGroup.notify(queue: .main) {
+    print("Second completed")
+}
+
+print("ВЫШЕ ПОСЛЕДНеГО")
+
+secondGroup.wait() //пока не выполнится
